b960fd9efb61332ab6f519b3c7d9fef9c4c4c9a4
maniaabdi <mania.abdi287@gmail.com>
2019-10-14 02:05:35 +0000

    Some updates

--- tpch/pig/run_full_benchmark.sh
+++ tpch/pig/run_full_benchmark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 GREEN='\033[1;32m'
 NC='\033[0m'
@@ -7,10 +7,13 @@ NC='\033[0m'
 #./benchmark_tpch.sh 4 alluxio://neu-5-1:19998/tpch/in /user/root/tpch/out_alx 1 ALLUXIO HDFS | tee ALLUXIO_HDFS_8wrk.log
 
 echo "${GREEN} Run Input S3, Output HDFS (local)${NC}"
-./benchmark_tpch.sh 5 s3a://data/tpch-1G /tpch/q6_1g_5rep_s3 1 S3 HDFS | tee results/q6_1g_5rep_s3.log
+for ((  j = 1 ;  j <= 5;  j++  ))
+do
+     ./run_tpch.sh s3a://data/tpch-8G /tpch/q6_8g_1rep_s3 1 S3
+done
 
 #echo "${GREEN} Run Input HDFS, Output HDFS (local)${NC}"
-#./benchmark_tpch.sh 1 /tpch/tpch/in /user/root/tpch/out_4rep_hdfs4 1 HDFS HDFS | tee HDFS_HDFS_4wrk_1rep.log
+#./run_tpch.sh /tpch-pig/in /tpch-pig/out 1 HDFS  | tee pig-mapreduce-1G.log
 
 #echo "${GREEN} Run Input Alluxio, Output HDFS (local)${NC}"
 #./benchmark_tpch.sh 1 alluxio://neu-5-1:19998/tpch/in /user/root/tpch/out_4rep_alx 1 ALLUXIO HDFS | tee ALLUXIO_HDFS_4wrk_4rep.log
